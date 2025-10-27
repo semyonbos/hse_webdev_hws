aa68a73aded5197889f2e3ced4a4a7b6cfa599a5
semyonbos <sbosonogov1@yandex.ru>
2025-10-27 14:55:18 +0300

    hw2 final update

--- hw2_js_basic/hw_js_basic.js
+++ hw2_js_basic/hw_js_basic.js
@@ -1,4 +1,4 @@
-// скрипт для отзывчивой смены дизайна и мобильного меню
+// JS basic update: скрипт для отзывчивой смены дизайна и мобильного меню
 // Mobile menu
 const menuToggle = document.querySelector('.mobile-menu-toggle');
 const navMenu = document.querySelector('.nav-menu');
